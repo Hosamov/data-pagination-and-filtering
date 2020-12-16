c221a331cac910ae0283bf473b077e1b959b6416
Hosamov <hosamov@hotmail.com>
2020-12-15 18:49:29 -0800

    updated code in script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@ By: Matt Coale
 */
 
 //Declare global variables
-const studentList = document.querySelector('.student-list');
-const linkList = document.querySelector('.link-list');
-const itemsPerPage = 9; //Define max students to be shown per page
+const itemsPerPage = 9; //Define max number of students to be shown per page
 
 /*
 Create the `showPage` function
@@ -17,7 +15,8 @@ function showPage(list, page) {
   const startIndex = (page * itemsPerPage) - itemsPerPage; //Start index of data/students for the current page
   const endIndex = (page * itemsPerPage); //Declare end index of data/students for the current page
 
-  studentList.innerHTML = ''; //Set to empty string to remove any students previously displayed
+  const studentList = document.querySelector('.student-list'); //assign .student-list class to a variable
+  studentList.innerHTML = ''; //Set studentList to empty string to remove any students previously displayed
 
   //Loop over the list parameter
   for (let i = 0; i < list.length; i++) { //List all students from data.js
@@ -45,12 +44,12 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(list) {
-  const buttonsNum = list.length; //declare a variable to hold value
-  const listLength = buttonsNum / itemsPerPage; //Perform, store calculation for number of buttons needed
+  const numOfPages = Math.ceil(list.length / itemsPerPage); //declare a variable to calc number of pages needed
+  const linkList = document.querySelector('.link-list'); //assign .link-list class to variable
 
-  linkList.innerHTML = ''; //Set to empty string to remove any pagination buttons previously displayed
+  linkList.innerHTML = ''; //Set linkList to empty string to remove any pagination buttons previously displayed
 
-  for (let i = 0; i < listLength; i++) { //Add pagination to the app, iterating over listLength
+  for (let i = 0; i < numOfPages; i++) { //Add pagination to the app, iterating over listLength
     //Using insertAdjacentHTML() method, dynamically add individual pages into index.html
     linkList.insertAdjacentHTML('beforeend', `
       <li>
@@ -59,27 +58,25 @@ function addPagination(list) {
     `);
   }
 
-  const li = linkList.firstElementChild;
-  const firstButton = li.firstElementChild;
-  firstButton.className = 'active'; //Set the first button (1) as 'active'
-}
+  const li = linkList.firstElementChild; //get first child (li) of linkList
+  const firstButton = li.firstElementChild; //get first child (button) of li for editing
+  firstButton.className = 'active'; //Set the first button (1) as 'active' initially
 
-//Event Listener to track when a button is individually clicked by the user
-linkList.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const activeButton = e.target;
-    const activeClass = document.querySelector('.active');
-    activeClass.classList.remove('active'); //Remove all classnames of "active"
+  //Event Listener to track when a button is individually clicked by the user
+  linkList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const activeButton = e.target;
+      const activeClass = document.querySelector('.active');
 
-    //Check to see whether the button is 'active'
-    if (activeButton.className != 'active') {
-      activeButton.className = 'active';  //if it isn't active, set it to 'active'
+      activeClass.className = ''; //Change class of 'active' to an empty string
+      activeButton.className = 'active'; //Set the target button to 'active'
+
       let activePage = activeButton.textContent; //See what button is being clicked
       showPage(data, activePage); //pass values of data (data.js) and activePage to function, showPage()
     }
-  }
-});
+  });
+}
 
 // Call functions
-showPage(data, 1); //By default, start on first page
+showPage(data, 1); //By default, start on first page, showing 'data' list from beginning
 addPagination(data); //Include list from data.js to calculate how many pages are required
